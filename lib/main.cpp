cb1a50711fc4623f71ccbc45d55b89d854f597bf
Brunocdti <brunocdtimoteo@gmail.com>
2025-02-08 20:21:03 -0300

    Update main.cpp

--- lib/main.cpp
+++ lib/main.cpp
@@ -17,10 +17,31 @@ SparseMatrix multiplicacao(SparseMatrix& a, SparseMatrix& b){
 }
 
 //recebe uma matriz vazia e le o arquivo de texto usando o fstream
-SparseMatrix lerMatris(){
+void readSparseMatrix(SparseMatrix& m, const std::string& nomeArquivo) {
+        //Abre o arquivo especificado pelo nome nomeArquivo e verifica se ele foi aberto corretamente.
+        std::ifstream arquivo(nomeArquivo);
 
-}
+        if (!arquivo) {
+            std::cerr << "Erro ao abrir o arquivo: " << nomeArquivo << std::endl;
+            return;
+        }
+        //Lê a primeira linha do arquivo, que contém os valores linhas e colunas.
+        int linhas, colunas;
+        arquivo >> linhas >> colunas;
 
+        // Atualiza as dimensões da matriz
+        m.Linha = linhas;
+        m.Coluna = colunas;
+
+        // Lê as triplas (i, j, valor) do arquivo e insere os elementos na matriz.
+        int i, j;
+        double valor;
+        while (arquivo >> i >> j >> valor) {
+            m.insert(i, j, valor);
+        }
+
+        arquivo.close();
+    }
 //main
 int main () {
 
